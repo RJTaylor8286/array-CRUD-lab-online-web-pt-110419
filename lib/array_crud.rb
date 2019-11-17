ee3974ee8a99e52d98f580adb93d750862feee77
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-11-17 07:18:23 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,23 @@
-def create_an_empty_array
-  
+favorite_video_games = ["NBA2K", "God of War", "Zelda"]
 end
 
-def create_an_array
-  
+favorite_video_games << "Megaman"
 end
 
-def add_element_to_end_of_array(array, element)
-  
+favorite_video_games.unshift "Mario"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+mario = favorite_video_games.pop
 end
 
-def remove_element_from_end_of_array(array)
-  
+favorite_video_games.shift  
 end
 
-def remove_element_from_start_of_array(array)
-  
+favorite_video_games[2]  
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+favorite_video_games[0]
 end
 
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
+favorite_video_games[-1]  
 end
